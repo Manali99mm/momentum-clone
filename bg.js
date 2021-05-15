967d0e01e7d1c648bf13b00099a4bcfdf574ca8c
Manali Mathur <a.manalimathur@gmail.com>
2021-05-16 01:05:14 +0530

    Unsplash API for bg image

--- bg.js
+++ bg.js
@@ -1,5 +1,6 @@
 const body = document.querySelector("body");
 const client_id = config.UNSPLASH_ID;
+
 const URL = `https://api.unsplash.com/search/photos?query=landscape&per_page=50&client_id=${client_id}`;
 
 fetch(URL)
